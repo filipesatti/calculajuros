157ae7e4dd940eb05c0de9af0f459847783f2446
filipesatti <filipesatti.sl@gmail.com>
2019-04-27 14:55:35 -0300

    Initial commit - project CalculaJuros for docker
    Initial commit - project CalculaJuros for docker

---
+++ src/CalculaJuros/Controllers/TesteController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculaJuros.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/")]
+    public class TesteController : Controller
+    {
+        // GET: api/Teste/5
+        [HttpGet("calculajuros")]
+        public string GetCalculaJuros(double valorinicial,int meses)
+        {
+            return Math.Pow(valorinicial*(1.01), meses).ToString();
+        }
+
+        //ShowMeTheCode
+        [HttpGet("showmethecode")]
+        public string ShowMeTheCode()
+        {
+            return "value";
+        }
+    }
+}
